dfc8f1c8356f3d0d7cc012e1f4603e2e37d16c90
Roman <qardolgopolyi.noveo@gmail.com>
2020-01-23 16:04:34 +0300

    added parametrize link

--- test_product_page.py
+++ test_product_page.py
@@ -1,28 +1,17 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_cart()
     product_page.solve_quiz_and_get_code()
     product_page.should_be_adding_confirmation()
-
-
-def test_compare_price(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_to_cart()
-    product_page.solve_quiz_and_get_code()
     product_page.compare_price()
-
-
-def test_compare_book_name(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_to_cart()
-    product_page.solve_quiz_and_get_code()
     product_page.compare_name()
